ea10b5e9396ece0a9a6256a3366656c039bd8848
AlmaTes <matej.manczal0@gmail.com>
2022-08-08 13:50:27 +0200

    flask init

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("Hello world")
-print("Hello world")
-print("Hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run()
