2e4da78341ee385a998f1f9b443b051b3ed40c67
HvezdaFlexu <dalicek2004@seznam.cz>
2022-08-08 16:02:44 +0200

    funkcni dotaznik od daliho

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    print("neco napisu")
     return "<p>Hello, World!</p>"
+
+@app.route("/randomendpoint")
+def sadfasdfsaparek():
+    return str(8*4*1000)
+
+@app.route("/klacek/<karek>")
+def parek(karek):
+    return karek
+
+@app.route("/polican",methods=["GET","POST"])
+def vysocina():
+    if request.method=="POST":
+        for i in ["username","username1","username2","username3"]:
+            with open('uzeniny.txt', 'a') as f:
+                f.write(str(request.form[i])+"\n")
+                f.close()
+
+    else:
+        return render_template("test.html")
+
 
 if __name__ == '__main__':
     app.run()
