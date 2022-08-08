aa571fa7bad3a6c220ea45198bf0b6614f342a62
HvezdaFlexu <dalicek2004@seznam.cz>
2022-08-08 16:04:29 +0200

    refaktor

--- main.py
+++ main.py
@@ -2,20 +2,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    print("neco napisu")
-    return "<p>Hello, World!</p>"
-
-@app.route("/randomendpoint")
-def sadfasdfsaparek():
-    return str(8*4*1000)
-
-@app.route("/klacek/<karek>")
-def parek(karek):
-    return karek
-
-@app.route("/polican",methods=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def vysocina():
     if request.method=="POST":
         for i in ["username","username1","username2","username3"]:
